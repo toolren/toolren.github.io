e3bfcf62a0d3f140be7c75fffcb76bdc4feb9f38
bytezou <280576059@qq.com>
2025-10-24 18:51:43 +0800

    feat: test

--- astro.config.js
+++ astro.config.js
@@ -19,7 +19,7 @@ import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://drowsyindra.github.io',
+  site: 'https://toolren.github.io',
   integrations: [tailwind(), react(), sitemap()],
   markdown: {
     syntaxHighlight: false,
